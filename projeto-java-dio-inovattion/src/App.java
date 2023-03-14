4f3998a61815683b43fb799792e09676d5ad43b6
Gabrielly <gabrielly_cristhine@outlook.com>
2023-03-14 20:42:20 -0300

    iniciaçao java

--- projeto-java-dio-inovattion/src/App.java
+++ projeto-java-dio-inovattion/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Bem Vindos Turma Dio!");
+        System.out.println("Bem Vindos Turma Dio Inovattion Iniciaçao em Java!");
     }
 }

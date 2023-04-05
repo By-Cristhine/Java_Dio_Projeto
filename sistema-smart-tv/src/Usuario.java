c576354ce9c01c351f26fa6a69e766e689913581
Gabrielly <gabrielly_cristhine@outlook.com>
2023-04-05 00:40:11 -0300

    Atualizando Smart

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -3,10 +3,23 @@ public class Usuario {
         
         SmartTv smartTv = new SmartTv();
 
-        System.out.println("Tv Ligada ?" + smartTv.ligada);
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.mudarCanal(35);
+
         System.out.println("Canal Atual :" + smartTv.canal);
         System.out.println("Volume Atual :" +  smartTv.volume);
-
+        System.out.println("Tv Ligada ?" + smartTv.ligada);
         
+    
+        smartTv.ligar();
+        System.out.println("Novo status -> Tv Ligada?" + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status -> Tv ligada?" + smartTv.ligada);
     }
 }
